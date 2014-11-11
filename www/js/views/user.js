6c0d50ee3363a0ef168169f9912b9fbc563fc8de
oscar <oscar@wishlist.dev>
2014-11-11 04:17:45 +0100

    Install datepicker and mask jquery

--- www/js/views/user.js
+++ www/js/views/user.js
@@ -8,4 +8,23 @@ $(document).ready(function() {
 		console.log($(element));
 		$(element).fadeIn('slow');
 	});
+
+	$('#date').datepicker({ 
+		altField: "#date",
+		closeText: 'Fermer',
+		prevText: 'Précédent',
+		nextText: 'Suivant',
+		currentText: 'Aujourd\'hui',
+		monthNames: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+		monthNamesShort: ['Janv.', 'Févr.', 'Mars', 'Avril', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
+		dayNames: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+		dayNamesShort: ['Dim.', 'Lun.', 'Mar.', 'Mer.', 'Jeu.', 'Ven.', 'Sam.'],
+		dayNamesMin: ['Di', 'Lu', 'Ma', 'Me', 'Je', 'Ve', 'Sa'],
+		weekHeader: 'Sem.',
+		dateFormat: 'dd-mm-yy',
+		firstDay: 1
+
+	}).mask("99-99-9999");
+;
+
 });
